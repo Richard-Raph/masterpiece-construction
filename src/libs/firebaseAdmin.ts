869543315298d94a5c0871637cabfac47bc5e039
Richard-Raph <richardakpan77@gmail.com>
2025-04-21 22:19:43 +0100

    `Revert clientEmail removal and add newline at end of file`

--- src/libs/firebaseAdmin.ts
+++ src/libs/firebaseAdmin.ts
@@ -1,13 +1,13 @@
 import { getAuth } from 'firebase-admin/auth';
+import serviceAccount from './serviceAccount.json';
 import { getFirestore } from 'firebase-admin/firestore';
 import { cert, getApps, initializeApp } from 'firebase-admin/app';
-import serviceAccount from './serviceAccount.json'
 
 // Transform the JSON to match the ServiceAccount interface
 const serviceAccountConfig: import('firebase-admin').ServiceAccount = {
     projectId: serviceAccount.project_id,
-    clientEmail: serviceAccount.client_email,
     privateKey: serviceAccount.private_key,
+    clientEmail: serviceAccount.client_email,
 };
 
 const firebaseAdminConfig = {
@@ -16,7 +16,7 @@ const firebaseAdminConfig = {
 
 const app = getApps().length === 0 ? initializeApp(firebaseAdminConfig) : getApps()[0];
 console.log('Firebase Admin initialized for project:', app.options.projectId);
-const adminAuth = getAuth(app);
 const adminDb = getFirestore(app);
+const adminAuth = getAuth(app);
 
 export { adminAuth, adminDb };
