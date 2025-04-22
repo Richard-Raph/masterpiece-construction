bf40db08849e08c7e0f51779bc7d140961d8fae4
Richard-Raph <richardakpan77@gmail.com>
2025-04-22 10:10:40 +0100

    `feat: updated next.config.ts, Input.tsx, firebase.ts, and firebaseAdmin.ts files with various changes`

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,16 @@
-import { FaLock, FaEnvelope, FaTag, FaDollarSign, FaFileAlt, FaUser } from 'react-icons/fa';
+import { useState } from 'react';
+import { FaEye, FaTag, FaLock, FaUser, FaFileAlt, FaEyeSlash, FaEnvelope, FaDollarSign } from 'react-icons/fa';
 
 interface InputProps {
     id: string;
     name: string;
     type?: string;
+    rows?: number;
     label: string;
     value: string;
-    rows?: number;
     textarea?: boolean;
     placeholder: string;
+    onClick: (e: React.MouseEvent<HTMLInputElement>) => void;
     onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
     icon?: 'text' | 'email' | 'password' | 'productName' | 'productPrice' | 'productDescription';
 }
@@ -19,12 +21,19 @@ export default function Input({
     name,
     label,
     value,
+    onClick,
     rows = 3,
     onChange,
     placeholder,
     type = 'text',
     textarea = false,
 }: InputProps) {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePassword = () => {
+        setShowPassword(prev => !prev);
+    };
+
     const Icon = () => {
         switch (icon) {
             case 'productPrice':
@@ -45,7 +54,7 @@ export default function Input({
     };
 
     return (
-        <div className="mb-6">
+        <div className="mb-6 relative">
             <label htmlFor={id} className="text-sm font-medium text-mp-dark mb-2 flex items-center">
                 <Icon />
                 {label}
@@ -61,18 +70,20 @@ export default function Input({
                     placeholder={placeholder}
                     className="w-full p-3 border outline-none text-mp-gray border-gray-300 rounded-md focus:ring-1 focus:ring-[#f6c834] focus:border-transparent"
                 />
-            ) : (
+            ) : (<>
                 <input
                     id={id}
                     required
                     name={name}
-                    type={type}
                     value={value}
+                    onClick={onClick}
                     onChange={onChange}
                     placeholder={placeholder}
+                    type={icon === 'password' ? (showPassword ? 'text' : 'password') : type}
                     className="w-full p-3 border outline-none text-mp-gray border-gray-300 rounded-md focus:ring-1 focus:ring-[#f6c834] focus:border-transparent"
                 />
-            )}
+                {icon === 'password' && (showPassword ? <FaEyeSlash onClick={togglePassword} className="absolute right-5 bottom-4 cursor-pointer hover:text-[#e0bb4b]" /> : <FaEye onClick={togglePassword} className="absolute right-5 bottom-4 cursor-pointer hover:text-[#e0bb4b]" />)}
+            </>)}
         </div>
     );
 }
--- src/libs/firebaseAdmin.ts
+++ src/libs/firebaseAdmin.ts
@@ -4,9 +4,9 @@ import { cert, getApps, initializeApp } from 'firebase-admin/app';
 
 // Initialize Firebase Admin with credentials from environment variables
 const serviceAccountConfig = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    projectId: process.env.FIREBASE_ADMIN_PROJECT_ID ?? '',
+    clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL ?? '',
+    privateKey: (process.env.FIREBASE_ADMIN_PRIVATE_KEY ?? '').replace(/\\n/g, '\n'),
 };
 
 const firebaseAdminConfig = {
@@ -15,7 +15,6 @@ const firebaseAdminConfig = {
 
 // Initialize Firebase app if not already initialized
 const app = getApps().length === 0 ? initializeApp(firebaseAdminConfig) : getApps()[0];
-console.log('Firebase Admin initialized for project:', app.options.projectId);
 
 const adminDb = getFirestore(app);
 const adminAuth = getAuth(app);
